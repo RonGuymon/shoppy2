bbf7d655f9c124fdc429ac9625f0bb3cadee01f9
RonGuymon <rnguymon@gmail.com>
2018-05-21 14:07:09 -0600

    Hourly add in.

--- R/getShoppyOrders.R
+++ R/getShoppyOrders.R
@@ -7,31 +7,77 @@
 #' @param shopifyPath Something like: mywebsite.com
 #' @param apiKey Unencoded api key
 #' @param apiPassword Unencoded apiPassword
+#' @param count Defaults to False. If set to True, it will only return the count of orders.
 #' @param verbose Whether it will return the results of the api call. Defaults to T.
+#' @param createdMin Show orders created at or after date (format: 2014-04-25T16:15:47-04:00).
+#' @param createdMax Show orders created at or before date (format: 2014-04-25T16:15:47-04:00).
 
-#' @return A dataframe of orders. This dataframe contains nested values.
+#' @return If count == T, then a number. Otherwise, a dataframe of orders. This dataframe may contain nested values.
 #' @export
 #'
-getShoppyOrders <- function(shopifyPath, apiKey, apiPassword, verbose = T){
+getShoppyOrders <- function(shopifyPath, apiKey, apiPassword, count = F, verbose = T, createdMin = NULL, createdMax = NULL){
   shopifyApiKey <- paste0(apiKey,":",apiPassword) %>% jsonlite::base64_enc() %>% gsub("[\r\n]", "", .) %>% paste0("Basic ", .)
 
   # Ping the api
-  if(verbose == T){
-    r <- GET(paste0("https://", shopifyPath, "/admin/orders.json?limit=250"),
-             add_headers("Authorization" = shopifyApiKey
-                         , "Content-Type" = "application/json"
-             )
-             ,verbose()
-    )
-  }else{
-    r <- GET(paste0("https://", shopifyPath, "/admin/orders.json?limit=250"),
-             add_headers("Authorization" = shopifyApiKey
-                         , "Content-Type" = "application/json"
-             )
-    )
-  }
+  if(count == T){
+    if(verbose == T){
+      r <- GET(paste0("https://", shopifyPath, "/admin/orders/count.json"),
+               add_headers("Authorization" = shopifyApiKey
+                           , "Content-Type" = "application/json"
+               )
+               ,verbose()
+      )
+    }else{
+      r <- GET(paste0("https://", shopifyPath, "/admin/orders/count.json"),
+               add_headers("Authorization" = shopifyApiKey
+                           , "Content-Type" = "application/json"
+               )
+      )
 
-  # Parse the data and return a dataframe
-  df <- content(r, "text") %>% jsonlite::fromJSON() %>% as.data.frame()
+    }
+    df <- content(r, "parse")
+  }else if(count == F){
+    # All orders
+    if(is.null(createdMin) & is.null(createdMax)){
+
+      if(verbose == T){
+        r <- GET(paste0("https://", shopifyPath, "/admin/orders.json?limit=250"),
+                 add_headers("Authorization" = shopifyApiKey
+                             , "Content-Type" = "application/json"
+                 )
+                 ,verbose()
+        )
+      }else{
+        r <- GET(paste0("https://", shopifyPath, "/admin/orders.json?limit=250"),
+                 add_headers("Authorization" = shopifyApiKey
+                             , "Content-Type" = "application/json"
+                 )
+        )
+      }
+
+      # Parse the data and return a dataframe
+      df <- content(r, "text") %>% jsonlite::fromJSON() %>% as.data.frame()
+    }else {
+      # Orders by hour
+
+      if(verbose == T){
+        r <- GET(paste0("https://", shopifyPath, "/admin/orders.json?limit=250&created_at_min=", createdMin, "&created_at_max=", createdMax),
+                 add_headers("Authorization" = shopifyApiKey
+                             , "Content-Type" = "application/json"
+                 )
+                 ,verbose()
+        )
+      }else{
+        r <- GET(paste0("https://", shopifyPath, "/admin/orders.json?limit=250&created_at_min=", createdMin, "&created_at_max=", createdMax),
+                 add_headers("Authorization" = shopifyApiKey
+                             , "Content-Type" = "application/json"
+                 )
+        )
+      }
+
+      # Parse the data and return a dataframe
+      df <- content(r, "text") %>% jsonlite::fromJSON() %>% as.data.frame()
+    }
+  }
   return(df)
 }
